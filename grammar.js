f8aebedc091f9893d171d15c848986df48a717ab
Michael Geddes <michaelg@striven.com.au>
2024-04-08 07:37:59 +0800

    Implement inline var for delphi

--- grammar.js
+++ grammar.js
@@ -119,6 +119,15 @@ function statements(trailing) {
 	let semicolon     = trailing ? [] : [';'];
 	
 	return Object.fromEntries([
+		[rn('var'),         $ => seq(
+			$.kVar,
+			$.identifier,
+			optional(seq(
+				':',
+				field('type', $.typeref)
+			)),
+			optional(seq($.kAssign, $._expr)
+		))],
 		[rn('if'),          $ => seq(
 			$.kIf, field('condition', $._expr), $.kThen,
 			field('then', lastStatement($))
@@ -245,6 +254,7 @@ function statements(trailing) {
 		[rn('_statement'),   $ => choice(
 			...semicolon,
 			seq($.assignment, ...semicolon),
+			seq($.varDef, ...semicolon),
 			alias($[rn('statement')], $.statement),
 			alias($[rn('if')],        $.if),
 			alias($[rn('ifElse')],    $.ifElse),
@@ -341,7 +351,7 @@ module.exports = grammar({
 		...statements(true),
 
 		assignment:      $ => op.infix(1,
-			$._expr,
+			choice($._expr, $.varAssignDef),
 			choice(
 				$.kAssign,
 				...enable_if(fpc,
@@ -350,11 +360,16 @@ module.exports = grammar({
 			),
 			$._expr
 		),
-
+		varAssignDef:          $ => seq($.kVar, $.identifier,
+			optional(seq(
+				':',
+				field('type', $.typeref)
+			))),
+		varDef:          $ => seq($.kVar, $.identifier, ':', field('type', $.typeref)),
 		label:           $ => seq($.identifier, ':'),
 		caseLabel:       $ => seq(delimited1(choice($._expr, $.range)), ':'),
 
-		_statements:     $ => repeat1(choice($._statement, $.label)),
+		_statements:     $ => repeat1(choice($.varDef, $._statement,  $.label)),
 		_statementsTr:   $ => seq(
 			repeat(choice($._statement, $.label)),
 			choice(tr($,'_statement'), $._statement)
